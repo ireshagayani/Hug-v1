d837cc26c2b0dc959799a43b8de1f918deea6aad
fdocharles <fdocharles@gmail.com>
2022-09-15 23:56:27 -0400

    Added Order and OrderItem models

---
+++ Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Order : BaseModel
+    {
+        public int OrderNo { get; set; }
+        public IList<OrderItem> OrderItems { get; set; }
+        public Decimal SubTotal { get; set; }
+        public Decimal Total { get; set; }
+    }
+}
---
+++ Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class OrderItem : BaseModel
+    {
+        public int Qty { get; set; }
+        public Decimal Price { get; set; }
+    }
+}
