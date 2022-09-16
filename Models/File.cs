3020bddd9216f875bb42da2e9bfc7cb8b1ffb088
fdocharles <fdocharles@gmail.com>
2022-09-16 00:07:52 -0400

    Added File model

---
+++ Models/File.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class File : BaseModel
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string FileType { get; set; }
+        public int EntityId { get; set; }
+        public string Base64Data { get; set; }
+    }
+}
