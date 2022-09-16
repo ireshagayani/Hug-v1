c5bc713b7f80383215443124b96fcb8c3071ff50
fdocharles <fdocharles@gmail.com>
2022-09-16 00:31:43 -0400

    Added Address, Client and Contact models

---
+++ Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Client : BaseModel
+    {
+        public string Name { get; set; }
+        public Contact Contact { get; set; }
+        public Address Address { get; set; }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Contact
+    {
+        public string MobilePhone { get; set; }
+        public string WorkPhone { get; set; }
+        public string Email { get; set; }
+    }
+}
